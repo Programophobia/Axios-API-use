4f79ec8a8f107edfbadead16f8820bf9843df0e1
Anna Powaska <150252110+Programophobia@users.noreply.github.com>
2024-06-12 09:40:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const search = document.querySelector('#yo');
 
+search.addEventListener('submit', async function(e){
+	e.preventDefault();
+	const input = search.elements.q.value
+	console.log(input);
+	const chill = await axios.get(`https://api.tvmaze.com/search/shows?q=${input}`)
+	console.log(chill.data[0].show.image.medium)
+	const img = document.createElement('img');
+	img.src = chill.data[0].show.image.medium
+	document.body.append(img)
+})
